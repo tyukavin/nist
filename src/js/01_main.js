d2a378e4d195bd5b966a836eae81025630b318b6
Denis Tyukavin <dtyukavin@dmvdesk.com>
2023-05-15 00:27:00 +0300

    edited site

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     let isMobile = false;
 
     if(window.matchMedia("(max-width: 767px)").matches){
-        
+
         isMobile = true;
     } else{
-        
+
         isMobile = false;
     }
 
@@ -113,14 +113,13 @@ $(document).ready(function() {
     });
 
     $('.js-slider-gallery').slick({
+        autoplay: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: true,
+        arrows: false,
         dots: true,
         fade: true,
-        prevArrow: '.btn-gallery.left',
-        nextArrow: '.btn-gallery.right',
-        infinite: false,
+        infinite: true,
     });
 
     $('.js-about-slider').slick({
@@ -135,23 +134,33 @@ $(document).ready(function() {
     });
 
     $('.tab-list__item').click(function() {
+
         $('.tab-list__item').removeClass('active');
         $('.tab-content__item').removeClass('active');
         $('.tab-content__item').eq($(this).index()).addClass('active');
         $(this).addClass('active');
+
+        $('.tab-content__footer .btn-collapse').text(
+            $('.tab-content__footer .btn-collapse').text() == 'Скрыть' ? 'Развернуть' : 'Развернуть'
+        );
+        $('.tab-content__footer').removeClass('grad-hidden');
+        $('.tab-content__main').removeClass('open');
     });
 
     $('.faq__head').click(function() {
+
         $(this).parent().toggleClass('active');
         $(this).parent().find('.faq__content').slideToggle();
     });
 
     $('.js-nist').click(function() {
+
         $(this).parent().parent().toggleClass('active');
         $(this).parent().parent().find('.nist__item-content').slideToggle();
     });
 
     $('.btn-param').click(function() {
+
         $(this).parent().parent().find('.params-hidden-content').slideToggle();
         $(this).find('span').text(
             $(this).find('span').text() == 'Подробнее' ? 'Скрыть' : 'Подробнее'
@@ -175,30 +184,107 @@ $(document).ready(function() {
     });
 
     $('.filter-mobile').on('touchend', function() {
+
         $('.filter').addClass('show');
         $('html').addClass('no-scroll');
     });
 
     $('.filter-back').on('touchend', function() {
+
+        $('.filter').removeClass('show');
+        $('html').removeClass('no-scroll');
+    });
+
+    $('.filter__action .btn--primary').on('touchend', function() {
+
         $('.filter').removeClass('show');
         $('html').removeClass('no-scroll');
     });
 
 
     $('.header .menu-mobile').on('touchend', function() {
+
         $('.menu').addClass('show');
         $('html').addClass('no-scroll');
     });
 
     $('.menu .close').on('touchend', function() {
+
         $('.menu').removeClass('show');
         $('html').removeClass('no-scroll');
     });
 
     $('.open-aside-mobile').on('touchend', function() {
+
         $(this).toggleClass('open');
         $(this).parent().toggleClass('open');
         $('html').toggleClass('no-scroll');
+    });
+
+    $('.btn-collapse').click(function() {
+
+        $(this).text(
+            $(this).text() == 'Развернуть' ? 'Скрыть' : 'Развернуть'
+        );
+        $(this).parent().toggleClass('grad-hidden');
+        $(this).parent().parent().find('.tab-content__main').toggleClass('open');
+    });
+
+    $(".btn-scroll-down").click(function() {
+
+        $('html, body').animate({
+            scrollTop: $(`#${$(this).data("scroll")}`).offset().top
+        }, 1000);
+    });
+
+    $(".js-btn-garant").click(function() {
+
+        $('html, body').animate({
+            scrollTop: $(`#${$(this).data("scroll")}`).offset().top - 100
+        }, 1000);
+    });
+
+    $(window).scroll(startCounter);
+
+    function startCounter() {
+
+        if ( $(".numbers").length === 0 ) return false;
+
+        const scrollY = $(".numbers").offset().top - window.innerHeight;
+
+        if ($(window).scrollTop() > scrollY) {
+
+            $(window).off("scroll", startCounter);
+
+            $(".number-item__num span").each(function () {
+
+                const $this = $(this);
+
+                jQuery({ Counter: 0 }).animate({ Counter: $this.attr("data-number") }, {
+                    duration: 2000,
+                    easing: "swing",
+                        step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+        }
+    }
+
+    $(".js-btn-search").click(function() {
+
+        $(this).toggleClass("active");
+        $(".js-search-form").toggle(200);
+        $(".js-search-input").focus();
+    })
+
+    $(document).click(function(event) {
+
+        if (!$(event.target).closest(".header-search").length) {
+
+            $(".js-search-form").hide();
+            $(".js-btn-search").removeClass("active");
+        }
     });
 
 });
